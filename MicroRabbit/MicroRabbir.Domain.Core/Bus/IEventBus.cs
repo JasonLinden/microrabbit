e98f3f405bb05f7eee6d5a682aa9cf4a8d52de36
Jason Linden <jason.linden@lexisnexis.co.za>
2022-01-15 20:57:56 +0200

    add interfaces and classses for domain core

---
+++ MicroRabbit/MicroRabbir.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T Command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, H>() where T : Event where H : IEventHandler<T>;
+    }
+}
